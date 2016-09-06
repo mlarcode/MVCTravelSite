b3243a29a2035d7173362d4952eee0f6878523e3
mlarcode <sightsnsenses@gmail.com>
2016-09-06 18:07:39 -0400

    first commit

---
+++ MVCReviewSite/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCReviewSite.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public virtual ICollection<Review> Review { get; set; }
+        public string Name { get; set; }
+    }
+}
